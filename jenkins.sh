e0690b6b19e2f802e68357706c409b163b82db27
Zeeshan Tariq <zeeshantariq.me@gmail.com>
2020-02-02 22:27:05 +0500

    DEBUG: jenkins file updated

--- jenkins.sh
+++ jenkins.sh
@@ -3,5 +3,11 @@ git fetch --all
 echo "fetching"
 
 git reset --hard origin/master
+git reset --hard upstream/master
+git pull origin master
 
 echo "pulled"
+
+composer dumpautoload
+
+echo "composer dumpautoload"
